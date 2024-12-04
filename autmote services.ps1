fe7ef0cb4e4315f288bd02adb3b6b539da03ea9a
Aaron Ashley <aaronashley466@gmail.com>
2024-12-04 15:46:53 -0700

    Add files via upload

---
+++ autmote services.ps1
@@ -0,0 +1,6 @@
+﻿   $files = Get-ChildItem -Path "C:\Logs" -Filter "*.log"
+   foreach ($file in $files) {
+       if ($file.LastWriteTime -lt (Get-Date).AddDays(-7)) {
+           Remove-Item $file.FullName -Force
+       }
+   }
